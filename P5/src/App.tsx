a8cd83a0e75372f1d0404bb109964b44642f502a
sarvesh <srbhanarkar05@gmail.com>
2026-01-16 20:50:21 +0530

    new file:   P5/.bundle/config 	new file:   P5/.eslintrc.js 	new file:   P5/.gitignore 	new file:   P5/.prettierrc.js 	new file:   P5/.watchmanconfig 	new file:   P5/Gemfile 	new file:   P5/README.md 	new file:   P5/__tests__/App.test.tsx 	new file:   P5/android/app/build.gradle 	new file:   P5/android/app/debug.keystore 	new file:   P5/android/app/proguard-rules.pro 	new file:   P5/android/app/src/main/AndroidManifest.xml 	new file:   P5/android/app/src/main/java/com/p5/MainActivity.kt 	new file:   P5/android/app/src/main/java/com/p5/MainApplication.kt 	new file:   P5/android/app/src/main/res/drawable/rn_edit_text_material.xml 	new file:   P5/android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   P5/android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   P5/android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   P5/android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   P5/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   P5/android/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   P5/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   P5/android/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   P5/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   P5/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   P5/android/app/src/main/res/values/strings.xml 	new file:   P5/android/app/src/main/res/values/styles.xml 	new file:   P5/android/build.gradle 	new file:   P5/android/gradle.properties 	new file:   P5/android/gradle/wrapper/gradle-wrapper.jar 	new file:   P5/android/gradle/wrapper/gradle-wrapper.properties 	new file:   P5/android/gradlew 	new file:   P5/android/gradlew.bat 	new file:   P5/android/settings.gradle 	new file:   P5/app.json 	new file:   P5/assests/Five.png 	new file:   P5/assests/Four.png 	new file:   P5/assests/One.png 	new file:   P5/assests/Six.png 	new file:   P5/assests/Three.png 	new file:   P5/assests/Two.png 	new file:   P5/babel.config.js 	new file:   P5/index.js 	new file:   P5/ios/.xcode.env 	new file:   P5/ios/P5.xcodeproj/project.pbxproj 	new file:   P5/ios/P5.xcodeproj/xcshareddata/xcschemes/P5.xcscheme 	new file:   P5/ios/P5/AppDelegate.swift 	new file:   P5/ios/P5/Images.xcassets/AppIcon.appiconset/Contents.json 	new file:   P5/ios/P5/Images.xcassets/Contents.json 	new file:   P5/ios/P5/Info.plist 	new file:   P5/ios/P5/LaunchScreen.storyboard 	new file:   P5/ios/P5/PrivacyInfo.xcprivacy 	new file:   P5/ios/Podfile 	new file:   P5/jest.config.js 	new file:   P5/metro.config.js 	new file:   P5/package-lock.json 	new file:   P5/package.json 	new file:   P5/src/App.tsx 	new file:   P5/src/index.d.ts 	new file:   P5/tsconfig.json

---
+++ P5/src/App.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View,ImageSourcePropType } from 'react-native'
+import React, { PropsWithChildren } from 'react'
+import DiceOne from '../assests/One.png'
+import DiceTwo from '../assests/Two.png'
+import DiceThree from '../assests/Three.png'
+import DiceFour from '../assests/Four.png'
+import DiceFive from '../assests/Five.png'
+import DiceSix from '../assests/Six.png'
+
+type DiceProps = PropsWithChildren<{
+  imageUrl:ImageSourcePropType
+}>
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>Nigger</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
