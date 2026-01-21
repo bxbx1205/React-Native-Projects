435125479decdf838944343540f04ff59f878b87
sarvesh <srbhanarkar05@gmail.com>
2026-01-21 20:23:25 +0530

    starting with music player project
    	modified:   __tests__/App.test.tsx
    	modified:   index.js
    	new file:   musicPlayerServices.js
    	modified:   package-lock.json
    	modified:   package.json
    	renamed:    App.tsx -> src/App.tsx
    	new file:   src/assets/audio/five.mp3
    	new file:   src/assets/audio/four.mp3
    	new file:   src/assets/audio/one.mp3
    	new file:   src/assets/audio/six.mp3
    	new file:   src/assets/audio/three.mp3
    	new file:   src/assets/audio/two.mp3
    	new file:   src/constants.ts

--- Spotify/index.js
+++ Spotify/index.js
@@ -1,9 +1,15 @@
 /**
  * @format
  */
+import TrackPlayer from 'react-native-track-player';
+import {playbackService} from './musicPlayerServices'
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
+import { playerbackService } from './musicPlayerServices';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playerbackService);
+
+
